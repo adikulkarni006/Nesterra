0c233ad229bbeb1df462b0caf5471e756f3851d3
Advait Kulkarni <advaitkulkarni006@gmail.com>
2025-05-28 16:50:54 +0530

    Add Project Files

---
+++ public/js/map.js
@@ -0,0 +1,18 @@
+mapboxgl.accessToken = mapToken;
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: "mapbox://styles/mapbox/streets-v12",
+    center: [73.8786, 18.5246],
+    zoom: 9,
+});
+
+const marker = new mapboxgl.Marker({ color: "red" })
+    .setLngLat([73.8786, 18.5246])
+    .setPopup(
+      new mapboxgl.Popup({ offset: 25 }).setHTML(
+        "<h6>Exact location provided after booking!</h6>"
+      )
+    )
+    .addTo(map);
+
